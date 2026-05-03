a04ca5d745ca0d06d8d2e51ffc3e5ca9c26996e7
Chanathip087 <chanathip.raksat@gmail.com>
2026-05-03 16:19:00 +0700

    a3_008

---
+++ TOI_ZERO/a3_008.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N, K; cin >> N >> K;
+    vector<int> C(N);
+    for (int i = 0; i < N; i++) cin >> C[i];
+    
+    
+}
